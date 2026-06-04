5b2365a89e07de81864e512bf3db156a2e434078
ReemxA <reemabdelmagid@outlook.com>
2026-06-05 01:29:04 +0200

    Move user endpoints into active controller

--- src/main/java/htw/webtech/pickmymovie/controller/MovieController.java
+++ src/main/java/htw/webtech/pickmymovie/controller/MovieController.java
@@ -2,10 +2,16 @@ package htw.webtech.pickmymovie.controller;
 
 import htw.webtech.pickmymovie.controller.dto.MovieResponse;
 import htw.webtech.pickmymovie.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import htw.webtech.pickmymovie.model.User;
+import htw.webtech.pickmymovie.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
 
 import java.util.List;
 
@@ -28,6 +34,19 @@ public class MovieController {
     @GetMapping("/users-test")
     public String usersTest() {
         return "users test works";
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
 
